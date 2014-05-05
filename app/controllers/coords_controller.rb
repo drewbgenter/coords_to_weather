b6590cd5182d0a217dc0cd7db814470c427e5b41
drewbgenter <drewbgenter@gmail.com>
2014-05-05 13:17:47 -0500

    weather final

--- app/controllers/coords_controller.rb
+++ app/controllers/coords_controller.rb
@@ -2,9 +2,25 @@ require 'open-uri'
 require 'json'
 
 class CoordsController < ApplicationController
+
   def fetch_weather
-    @latitude = 37.8267
-    @longitude = -122.423
+
+    if params[:address] == nil
+        @address = "the corner of foster and sheriden"
+    else
+        @address = params[:address]
+    end
+        @url_safe_address = URI.encode(@address)
+
+    # Your code goes here.
+    url1 = "http://maps.googleapis.com/maps/api/geocode/json?address=" + @url_safe_address + "&sensor=false"
+    raw_data1 = open(url1).read
+    parsed_data1 = JSON.parse(raw_data1)
+    @latitude = parsed_data1["results"][0]["geometry"]["location"]["lat"]
+    @longitude = parsed_data1["results"][0]["geometry"]["location"]["lng"]
+
+    #@latitude = 37.8267
+    #@longitude = -122.423
     your_api_key = "814bdab2953220a1d81f8f79702dd5bd"
 
     # Your code goes here.
