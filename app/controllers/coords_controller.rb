dfd159d7e98ccf1c06beb2e1f9310625f745fab0
drewbgenter <drewbgenter@gmail.com>
2014-05-05 13:29:58 -0500

    weather final

--- app/controllers/coords_controller.rb
+++ app/controllers/coords_controller.rb
@@ -13,11 +13,17 @@ class CoordsController < ApplicationController
         @url_safe_address = URI.encode(@address)
 
     # Your code goes here.
-    url1 = "http://maps.googleapis.com/maps/api/geocode/json?address=" + @url_safe_address + "&sensor=false"
-    raw_data1 = open(url1).read
-    parsed_data1 = JSON.parse(raw_data1)
-    @latitude = parsed_data1["results"][0]["geometry"]["location"]["lat"]
-    @longitude = parsed_data1["results"][0]["geometry"]["location"]["lng"]
+    #url1 = "http://maps.googleapis.com/maps/api/geocode/json?address=" + @url_safe_address + "&sensor=false"
+    #raw_data1 = open(url1).read
+    #parsed_data1 = JSON.parse(raw_data1)
+    #@latitude = parsed_data1["results"][0]["geometry"]["location"]["lat"]
+    #@longitude = parsed_data1["results"][0]["geometry"]["location"]["lng"]
+    url = "http://maps.googleapis.com/maps/api/geocode/json?address=" + @url_safe_address + "&sensor=false"
+    raw_data = open(url).read
+    parsed_data = JSON.parse(raw_data)
+    @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+    @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
+
 
     #@latitude = 37.8267
     #@longitude = -122.423
